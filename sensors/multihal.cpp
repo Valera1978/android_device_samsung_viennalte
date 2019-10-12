ca68358af72ffbf11bb87ee28594ad45f54207a6
Valera1978 <valera1978@tut.by>
2019-10-12 09:19:41 +0300

    add missed pthread_mutex_unlock

--- sensors/multihal.cpp
+++ sensors/multihal.cpp
@@ -158,6 +158,7 @@ void *writerTask(void* ptr) {
                 ALOGV("writerTask ignored error %d from %s", eventsPolled, device->common.module->name);
                 ALOGE("ERROR: Fix %s so it does not return error from poll()", device->common.module->name);
             }
+            pthread_mutex_unlock(&queue_mutex);
             continue;
         }
         pthread_mutex_lock(&queue_mutex);
@@ -515,6 +516,7 @@ static int device__close(struct hw_device_t *dev) {
     if (ctx != NULL) {
         int retval = ctx->close();
         delete ctx;
+        pthread_mutex_unlock(&init_modules_mutex);
         return retval;
     }
 
