4cb37e1d9ba76b182058da315b1c99ac8f48a285
Valera1978 <valera1978@tut.by>
2019-11-04 14:03:30 +0300

    update repopicks

--- picks.sh
+++ picks.sh
@@ -1,12 +1,6 @@
-# adb
-#repopick 255667
-
 # LineageOS/android_art
-repopick 256444
-
-# LineageOS/android_build
-#repopick 256458 256500 257099
+#repopick 256444
 
 # LineageOS/android_bionic
-#repopick 256168 256083 256147-256150
-repopick 256150
+#repopick 256150
+repopick 262885
